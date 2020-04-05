3b23279355c0e379970bd622e583f20ae650f2ef
KangMinseok <qbq5000@gmail.com>
2020-04-05 09:45:31 +0900

    Initial Commit

---
+++ SwiftUI_Book/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Book
+//
+//  Created by 강민석 on 2020/04/05.
+//  Copyright © 2020 MinseokKang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
